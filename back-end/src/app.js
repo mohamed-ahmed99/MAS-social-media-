4cd2c8c9b2f9a88649f2d81b3fa9fc2ea9d0e8e9
Mohamed Ahmed <masmhdmas@gmail.com>
2025-10-09 15:33:11 +0300

    server

---
+++ back-end/src/app.js
@@ -0,0 +1,54 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import mongoose from 'mongoose'
+
+
+const app = express() 
+dotenv.config()
+
+const allowedOrigins = process.env.ORIGINS.split(',') || []
+app.use(cors({
+    origin: (origin, callBack) => {
+        if(!origin || allowedOrigins.includes(origin)) return callBack(null, true)
+        else return callBack(new Error("NOT allowed by CORS"))
+    },
+    methods:["POST", "GET", "PUT", "PATCH","DELETE"],
+    credentials:true
+}))
+
+
+app.use(express.json())
+app.use(cookieParser())
+
+
+// connect with DB
+const ConnectDB = async () => {
+    try{
+        mongoose.set('strictQuery', false)
+        mongoose.connect(process.env.DB_URI)
+        console.log("DB done")
+    }
+    catch(error){
+        console.log(error.message)
+        process.exit(1)
+    }
+
+}
+ConnectDB()
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({message:"hello user"})
+})
+
+
+
+
+
+const Port = process.env.PORT
+app.listen(Port, () => console.log("server is runnning..."))
+
+// when deploy on vercel ✌️
+// export default app
