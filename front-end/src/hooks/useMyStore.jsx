024f1d8aab3a04c8914771b94caeeece83749908
Mohamed Ahmed <masmhdmas@gmail.com>
2025-10-11 23:19:39 +0300

    myStore hook

---
+++ front-end/src/hooks/useMyStore.jsx
@@ -0,0 +1,43 @@
+import { Children, createContext, useContext, useState } from "react"
+
+
+const StoreContext = createContext()
+
+
+export const AppProvider = ({children}) => {
+
+  const [store, setStoreState] = useState(() => {
+    const saved = sessionStorage.getItem("globalStore")
+    return saved ? JSON.parse(saved) : {}
+  });
+
+  const setStore = (key, value) => {
+    setStoreState((prev) => {
+        const updated = {...prev, [key]: value}
+        sessionStorage.setItem("globalStore", JSON.stringify(updated))
+        return updated
+    })
+  }
+  
+  return (
+    <StoreContext.Provider value={{store, setStore}}>
+        {children}
+    </StoreContext.Provider>
+  )
+}
+
+
+export const useMyStore = () => {
+  const context = useContext(StoreContext)
+  if (!context) {
+    throw new Error("useAppContext must be used inside an AppProvider")
+  };
+
+  return context
+
+}
+
+
+
+
+
