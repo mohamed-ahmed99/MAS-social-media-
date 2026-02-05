786e3bd650302e308d891a2171d60a262a0f2838
Mohamed <masmhdmas@gmail.com>
2026-02-05 05:17:53 +0200

    fix error

--- front-end/src/pages/SignIn.jsx
+++ front-end/src/pages/SignIn.jsx
@@ -45,7 +45,7 @@ const SignIn = () => {
             try{
                 // http://localhost:5150/api/auth/signin
                 // https://masproback.vercel.app/api/auth/signin
-                const res = await fetch("http://localhost:5150/api/auth/signin", {
+                const res = await fetch("https://masproback.vercel.app/api/auth/signin", {
                     method:"POST",
                     headers:{"content-type":"application/json"},
                     credentials:"include",
