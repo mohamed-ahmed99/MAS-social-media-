165188a4a8cd68ba71621250de1ba5d652cbe4c5
Mohamed <masmhdmas@gmail.com>
2026-02-16 15:12:05 +0200

    beack-end: Schema of Notifications

---
+++ back-end/src/models/notifications.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+
+const notificationSchema = new mongoose.Schema({
+    from:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"users",
+        required:true,
+        index:true,
+    },
+    to:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"users",
+        required:true,
+        index:true,
+    },
+
+    type:{type:String, required:true},
+
+    title:{type:String, required:true},
+
+    message:{type:String, required:true},
+
+    isRead:{type:Boolean, default:false},
+
+    readAt:Date,
+
+
+    
+}, {timestamps:true})
+
+
+
+notificationSchema.index({to:1, isRead:1, createdAt:-1})
+
+const Notifications = mongoose.model("notifications", notificationSchema)
+
+export default Notifications
