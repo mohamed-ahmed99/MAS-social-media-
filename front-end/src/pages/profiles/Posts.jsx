1ca601cb869cee97500b25c535ba4f2c1a4d4690
Mohamed <masmhdmas@gmail.com>
2026-04-16 14:33:43 +0200

    feat: load more posts

--- front-end/src/pages/profiles/Posts.jsx
+++ front-end/src/pages/profiles/Posts.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 // components
 import Post from "../../components/post/Post.jsx"
+import LoadMoreBtn from "../../components/btns/LoadMore.jsx"
 
 import { useGetMethod } from "../../hooks/useGetMethod.js";
 
@@ -9,42 +10,47 @@ import { useGetMethod } from "../../hooks/useGetMethod.js";
 
 export default function Posts({userId}) {
     const { getData, status_g, message_g, data_g, loading_g, action_g } = useGetMethod()
-    const [query, setQuery] = React.useState({limit: 10, page: 1})
+    const [query, setQuery] = React.useState({limit: 20, page: 1})
+
+    const [posts, setPosts] = React.useState([])
 
     // get user posts
     React.useEffect(() => {
         if (userId) {
             getData(`/api/posts/get/user/${userId}?limit=${query.limit}&page=${query.page}`)
+        }else{
+            console.log("no user id")
         }
     }, [query.page, query.limit, userId])
 
+    // set posts
+    React.useEffect(() => {
+        if (data_g?.posts) {
+            setPosts((prevPosts) => [...prevPosts, ...data_g.posts])
+        }
+    }, [data_g?.posts])
+
     console.log('posts', { status_g, message_g, data_g, loading_g, action_g })
   return (
-    <div>
+    <div >
       {loading_g ? (
         <div>Loading...</div>
       ) : (
-        data_g?.posts?.map((post) => (
-          <div key={post._id} className="bg-white lg:rounded-xl shadow-sm overflow-hidden border border-gray-100/50">
-             <Post data={post} canEdit={true}/>
-          </div>
-        ))
+        <div className='space-y-2'>
+            {posts?.map((post) => (
+                <Post data={post} canEdit={true} key={post._id}/>
+            ))}
+        </div>
+      )}
+
+      {data_g?.posts?.length > 0 && (
+        <LoadMoreBtn
+          loading={loading_g}
+          loadMore={() => setQuery({...query, page: query.page + 1})}
+          text="Load More"
+        />
       )}
     </div>
   )
 }
 
-
-
-// {posts && posts.length > 0 ? (
-//               posts.map((post) => (
-//                 <div key={post._id} className="bg-white lg:rounded-xl shadow-sm overflow-hidden border border-gray-100/50">
-//                    <Post data={post} />
-//                 </div>
-//               ))
-//             ) : ( 
-//               <NoPostsYet 
-//                 edit={edit} 
-//                 setCreatePost={setCreatePost} 
-//               />
-//             )}
